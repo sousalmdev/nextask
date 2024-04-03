6d3f26beca0a849bc4eaa0f8285b74547a7255a1
sousalmdev <leandromedeiros690@gmail.com>
2024-04-03 14:24:25 -0300

    Fix:Resolving contact section.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -15,50 +15,49 @@ import {
   Button,
   ChakraProvider,
 } from "@chakra-ui/react";
-import { Card, CardFooter } from "@chakra-ui/react";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-import { BiHome, BiLogoFacebook, BiLogoLinkedin, BiLaptop } from "react-icons/bi";
+import { color } from "framer-motion";
+import { BiLaptop, BiLayout, BiLogoFacebook, BiLogoLinkedin, BiSitemap } from "react-icons/bi";
 
 function Page() {
-  const onSubmit = (event: any) => {
+  
+
+  const onSubmit = (event:any) => {
     event.preventDefault()
     toast.success("Mail Successfully sent!");
   };
 
   return (
     <div className="flex flex-col items-center justify-center pt-20">
-      <Icon as={BiHome} className="md:hidden block" />
       <ToastContainer />
       <br />
       <ChakraProvider>
         <Text
           fontWeight={600}
+          display={"flex"}
+          width={"900px"}
           textAlign={"left"}
           color={"black"}
           fontSize={20}
-          mx={4}
-          mb={4}
         >
           Mail one of our representatives in the Number8 group:
         </Text>
-
+        <br />
         <Box
           backgroundColor={"black"}
           borderRadius={6}
           display="flex"
-          flexDirection={{ base: "column", md: "row" }}
-          justifyContent="center"
-          alignItems="center"
-          maxWidth={900}
-          mx={4}
+          width={900}
         >
           <Box
             borderRadius={6}
+            flexDirection={"column"}
             backgroundColor={"black"}
             color={"white"}
-            width={{ base: "full", md: "50%" }}
+            width={400}
             padding={6}
           >
             <Image
@@ -68,57 +67,51 @@ function Page() {
               src="https://cdn.number8.com/app/uploads/2023/06/number-8-logo-white.svg"
               alt="number-8"
             />
-        <form onSubmit={onSubmit}>
-  <Stack spacing={4}>
-    <FormControl>
-      <FormLabel>Name:</FormLabel>
-      <Input type="text" isRequired />
-      <FormHelperText fontWeight={500} color={"white"}>
-        Just first and second name if you want.
-      </FormHelperText>
-    </FormControl>
+            <form onSubmit={onSubmit}>
+              <FormControl>
+                <FormLabel>Name</FormLabel>
+                <Input type="text" isRequired />
+                <FormHelperText fontWeight={500} color={"white"}>
+                  Just first and second name if you want.
+                </FormHelperText>
+              </FormControl>
 
-    <FormControl>
-      <FormLabel>Email:</FormLabel>
-      <Input type="email" isRequired />
-      <FormHelperText fontWeight={500} color={"white"}>
-        We will never share your email.
-      </FormHelperText>
-    </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Email</FormLabel>
+                <Input type="email" isRequired />
+                <FormHelperText fontWeight={500} color={"white"}>
+                  We will never share your email.
+                </FormHelperText>
+              </FormControl>
 
-    <FormControl>
-      <FormLabel>Phone Number:</FormLabel>
-      <Input type="text" isRequired />
-      <FormHelperText fontWeight={500} color={"white"}>
-        We may call you to align some things.
-      </FormHelperText>
-    </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Phone Number</FormLabel>
+                <Input type="text" isRequired />
+                <FormHelperText fontWeight={500} color={"white"}>
+                  We may call you to align some things.
+                </FormHelperText>
+              </FormControl>
 
-    <FormControl>
-      <FormLabel>Subject:</FormLabel>
-      <Input type="text" isRequired />
-      <FormHelperText fontWeight={500} color={"white"}>
-        Tell us about your interest.
-      </FormHelperText>
-    </FormControl>
-  </Stack>
-
-  <Button mt={4} variant={"ghost"} colorScheme="blue" type="submit">
-    Submit
-  </Button>
-</form>
-
+              <FormControl mt={4}>
+                <FormLabel>Subject</FormLabel>
+                <Input type="text" isRequired />
+                <FormHelperText fontWeight={500} color={"white"}>
+                  Tell us about your interest.
+                </FormHelperText>
+              </FormControl>
+              <br />
+              <Button mt={4} variant={"ghost"} colorScheme="blue"  type="submit">
+                Submit
+              </Button>
+            </form>
           </Box>
-
           <Card
-            width={{ base: "full", md: "50%" }}
+            width={500}
             fontWeight={600}
             color={"white"}
             direction={"column"}
             align={"center"}
             justify={"center"}
-            padding={6}
-            backgroundColor={"none"}
           >
             <Image
               margin={"0 auto"}
@@ -126,18 +119,15 @@ function Page() {
               alt="image"
               className="h-10"
             />
-            <Text
-              align={"center"}
-              fontSize={20}
-              color={"white"}
-              mt={4}
-              mb={4}
-            >
+            <br />
+
+            <Text align={"center"} fontSize={20} color={"black"}>
               Thanks for trusting our job.
             </Text>
-            <Divider />
-            <CardFooter flexDirection={'column'} color={'white'} mt={4}>
+            <Divider/>
+            <CardFooter flexDirection={'column'} color={'black'}>
               Our Social Medias:
+         
               <Stack className="py-2" direction={'row'} spacing={6}>
                 <Link href='https://www.linkedin.com/company/number-8-it/'><Icon transition='.3s ease' _hover={{color:'rgb(0,140,255)'}} boxSize={'30px'} as={BiLogoLinkedin} /></Link>
                 <Link href='https://www.facebook.com/number8.dev/'><Icon transition='.3s ease' _hover={{color:'rgb(0,140,255)'}} boxSize={'30px'} as={BiLogoFacebook} /></Link>
